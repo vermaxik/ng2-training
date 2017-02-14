390950c91839a271817a79f84ae70d52df778deb
Maxim Verbovoy <vermaxik@gmail.com>
2017-02-14 13:55:49 +0200

    Working with a forms (FormGroup, FormBuilder)

--- app/task/task.module.ts
+++ app/task/task.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { TaskListComponent } from './task-list/task-list.component'
 import { TaskComponent } from './task/task.component'
 import { CommonModule } from "@angular/common";
+import { ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   declarations: [TaskComponent, TaskListComponent],
   exports: [TaskListComponent]
 })
--- app/task/task/task.component.ts
+++ app/task/task/task.component.ts
@@ -1,15 +1,32 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
-
+import { Component, Input, Output, EventEmitter, Inject, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   moduleId: module.id,
   selector: 'task',
   templateUrl: './task.component.html'
 })
-export class TaskComponent {
+export class TaskComponent implements OnInit {
   @Input() id:number;
   @Input() name:string;
   @Output() deleted:EventEmitter<number> = new EventEmitter();
+
+  taskForm: FormGroup;
+  defaultName: string;
+
+  constructor(@Inject(FormBuilder) private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.defaultName = this.name;
+    this.taskForm = this.fb.group({
+     name: [this.defaultName, Validators.required]
+    });
+  }
+
+  revert() {
+    this.taskForm.controls.name.setValue(this.defaultName);
+  }
+
 
   removed() {
     this.deleted.emit(this.id);
