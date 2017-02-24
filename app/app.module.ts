fcdb2a9fb5f7cd66f2b352b8f73d0456eadf5036
Maxim Verbovoy <vermaxik@gmail.com>
2017-02-24 19:22:33 +0200

    Added router to app

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,13 +1,22 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
 import { TaskModule } from './task/task.module';
 import { AppComponent } from './app.component';
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { TaskService } from './task/task.service';
+import { RouterModule } from "@angular/router";
+
+import { TaskComponent } from './task/task/task.component';
+
+let appRoutes = [{path: 'tasks', component: TaskComponent }];
 
 @NgModule({
-  imports: [BrowserModule, HttpModule, TaskModule, InMemoryWebApiModule.forRoot(TaskService)],
+  imports: [ BrowserModule,
+             HttpModule,
+             TaskModule,
+             InMemoryWebApiModule.forRoot(TaskService),
+             RouterModule.forRoot(appRoutes)],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
--- app/task/task.module.ts
+++ app/task/task.module.ts
@@ -3,9 +3,10 @@ import { TaskListComponent } from './task-list/task-list.component'
 import { TaskComponent } from './task/task.component'
 import { CommonModule } from "@angular/common";
 import { ReactiveFormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
 
 @NgModule({
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
   declarations: [TaskComponent, TaskListComponent],
   exports: [TaskListComponent],
   providers: []
